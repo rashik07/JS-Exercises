78fdc189b3f0e40f21441d8fbf8a68f72e761df4
rashik saif <saif67090707@gmail.com>
2018-05-22 19:11:07 +0000

    task2

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 /* JavaScript code here */
+function sum(input){
+             
+ if (toString.call(input) !== "[object Array]")
+    return false;
+      
+            var total =  0;
+            for(var i=0;i<input.length;i++)
+              {                  
+                if(isNaN(input[i])){
+                continue;
+                 }
+                  total += Number(input[i]);
+               }
+             return total;
+            }
+
