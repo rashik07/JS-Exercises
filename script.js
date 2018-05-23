dc18130784e52028db5570148de88c6203652183
rashik saif <saif67090707@gmail.com>
2018-05-23 02:56:54 +0000

    task5

--- script.js
+++ script.js
@@ -30,3 +30,27 @@ document.form1.text1.focus();
 return false;
 }
 }
+
+//task 5
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+function addItem(newItem){
+    cart.push(newItem);
+      console.log(cart);
+}
+var newItem= {name: "watch", price:64 , quantity:1};
+addItem(newItem);
